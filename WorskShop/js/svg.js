ecb32056239dc7dc858853e3481c5500fe2eeb7a
Benjdeul <43751383+Benjdeul@users.noreply.github.com>
2018-10-03 14:12:24 +0200

    Up

---
+++ WorskShop/js/svg.js
@@ -0,0 +1,15 @@
+$(function(){
+  $("#XMLID_1_").click(function(){
+    console.log('eneomr')
+    console.log($('#email').val())
+  });
+  $("#XMLID_2_").click(function(){
+    console.log("plop1");
+  });
+  $("#XMLID_3_").click(function(){
+    console.log("etage4");
+  });
+  $("#XMLID_4_").click(function(){
+    console.log("etage3");
+  });
+});
